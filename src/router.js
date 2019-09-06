03e1ea1226644bfd1a5c364316f3309b57a159e1
smelikecat <smelikecat@163.com>
2019-09-06 15:30:27 +0800

    import requires in main.js and router.js

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+import ChatDashboard from './vies/ChatDashboard.vue'
+
 
 Vue.use(Router)
 
@@ -10,16 +12,17 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'login',
+      component: Login
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/chat',
+      name: 'chat',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      // component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+	  components: ChatDashboard
     }
   ]
 })
